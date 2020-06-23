511b2f18ed666a0245cf2c5d0cab516b2604064f
Dallas Clarke <dallas.clarke@codeimmersives.com>
2020-06-22 21:29:42 -0700

    Homework

--- calculate-back-end.js
+++ calculate-back-end.js
@@ -2,6 +2,39 @@
  * YOUR CODE HERE *
  ******************/
 
+ function calculate (strOne, strTwo, strThree) {
+   if (strThree === "+" ) {
+     return parseInt(strOne) + parseInt(strTwo);
+   } else if (strThree === "-") {
+     return parseInt(strOne) - parseInt(strTwo);
+   } else if (strThree === "x") {
+     return parseInt(strOne) * parseInt(strTwo);
+   } else if (strThree === "/") {
+     return parseInt(strOne) / parseInt(strTwo);
+   } else if (strThree === "%") {
+     return parseInt(strOne) % parseInt(strTwo)
+   } else if (strThree === "plus") {
+     return parseInt(strOne) + parseInt(strTwo);
+   } else if (strThree === "minus") {
+     return parseInt(strOne) - parseInt(strTwo);
+   } else if (strThree === "X" || strThree === "times") {
+     return parseInt(strOne) * parseInt(strTwo)
+   } else if (strThree === "modulus" || strThree === "mod") {
+     return parseInt(strOne) % parseInt(strTwo);
+   } else if (strThree === "added to") {
+     return parseInt(strOne) + parseInt(strTwo);
+   } else if (strThree === "subtracted from") {
+     return parseInt(strOne) - parseInt(strTwo);
+   } else if (strThree === "multiplied by") {
+     return parseInt(strOne) * parseInt(strTwo);
+   } else if (strThree === "divided by") {
+     return parseInt(strOne) / parseInt(strTwo);
+   } else {
+     return "Sorry, that's not a mathematical operation!"
+   }
+   }
+
+ 
 
  
 
